348747e0fad2bd182dcc52b0987df757cffcaae0
AAkinkunmi <aakinkunmi@pursuit.org>
2024-09-23 12:54:35 -0400

    create users, connections, categories, subcategories tables

--- db/prod_schema.sql
+++ db/prod_schema.sql
@@ -13,15 +13,34 @@ DROP TABLE IF EXISTS messages;
 DROP TABLE IF EXISTS metrics;
 
 CREATE TABLE users (
-
+    id SERIAL PRIMARY KEY,
+    first_name TEXT NOT NULL,
+    last_name TEXT NOT NULL,
+    email TEXT,
+    password TEXT,
+    job_title TEXT,
+    signup_date TIMESTAMP
 );
 CREATE TABLE categories (
-
+    id SERIAL PRIMARY KEY,
+    name TEXT
 );
 CREATE TABLE subcategories (
-
+    id SERIAL PRIMARY KEY,
+    category_id INTEGER REFERENCES categories (id),
+    name TEXT
 );
 CREATE TABLE connections (
+    id SERIAL PRIMARY KEY,
+    mentor_id INTEGER REFERENCES users (id),
+    mentee_id INTEGER REFERENCES users (id),
+    category_id INTEGER REFERENCES categories (id),
+    subcategory_id INTEGER REFERENCES subcategories (id),
+    metric_one INTEGER,
+    metric_two INTEGER,
+    metric_three INTEGER,
+    metric_four INTEGER,
+    metric_five INTEGER,
 
 );
 CREATE TABLE assignments (
